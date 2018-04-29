ce763f0f49900cb78a691fc33779cbda87aa85cb
dalazaro <daryl.lazaro@gmail.com>
2018-04-29 15:12:10 -0700

    edited snippet formatting

--- src/components/body/snippets.js
+++ src/components/body/snippets.js
@@ -6,18 +6,17 @@ export const SnippetOne = () => {
   return(
     <div>
       <Highlight language="bash">
-        {`
-    $ marconi branch foo
-    >>> Success. Created branch foo.
+        {`$ marconi branch foo
+>>> Success. Created branch foo.
 
-    $ marconi status --branch_name=foo
-    >>> Branch foo: uninitialized
+$ marconi status --branch_name=foo
+>>> Branch foo: uninitialized
 
-    $ marconi init --config=/tmp/config.txt --branch_name=foo
-    >>> Success. Configured branch foo.
+$ marconi init --config=/tmp/config.txt --branch_name=foo
+>>> Success. Configured branch foo.
 
-    $ marconi run --branch_name=foo
-    >>> Success. Running branch foo.
+$ marconi run --branch_name=foo
+>>> Success. Running branch foo.
         `}
       </Highlight>
     </div>
